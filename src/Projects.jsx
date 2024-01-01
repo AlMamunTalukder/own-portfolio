5b9ce640aa826c7ad83214a89f95b3e2c7f5be11
AlMamunTalukder <almamuntalukder0@gmail.com>
2024-01-02 03:48:44 +0600

    portfolio update

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -6,15 +6,16 @@ const Projects = () => {
       </h1>
 
       <div className=" grid grid-cols-1 lg:grid-cols-3 gap-y-5 pb-8 px-4">
+        {/* project1 */}
         <div className="rounded-md lg:w-96 bg-gray-500 bg-opacity-20 shadow-xl ">
-          <figure>
+          <figure className="h-60">
             <img
               src="https://i.ibb.co/yynW5jK/newspaper.png"
               alt="Shoes"
               className="rounded-md"
             />
           </figure>
-          <div className="card-body">
+          <div className="card-body lg:-mt-5 h-56 ">
             <h2 className="card-title">News Paper Portal</h2>
             <p className="text-justify">
               This is a newpaper website name global Times. It has many more
@@ -22,44 +23,46 @@ const Projects = () => {
               login. Subscription system for premium user. Show articles, add
               articles, articles are difference for premium user and other user.
             </p>
-            <div className="flex gap-2 items-center text-center">
-              <a
-                href=" https://newspaper-final-assignment.netlify.app/"
-                className="btn-sm bg-green-400 rounded-full pt-1 lg:btn lg:bg-green-400 lg:rounded-full justify-start text-white lg:text-white "
-                rel="noopener noreferrer"
-              >
-                Live Link
-              </a>
-              <a
-                href="https://github.com/AlMamunTalukder/newspaper-client"
-                className="btn-sm bg-gray-400 rounded-full pt-1 lg:btn lg:bg-gray-400 text-white lg:text-black lg:rounded-full justify-end"
-                rel="noopener noreferrer"
-              >
-                Github
-              </a>
-            </div>
+          </div>
+          <div className="card-body flex-grow">
+            <a
+              href="https://newspaper-final-assignment.netlify.app/"
+              className="btn-sm bg-green-400 rounded-full pt-1 mb-1 lg:mb-0 lg:btn lg:bg-green-400 lg:rounded-full justify-start text-black lg:text-black lg:hover:text-white"
+              rel="noopener noreferrer"
+            >
+              Live Link
+            </a>
+            <a
+              href="https://github.com/AlMamunTalukder/newspaper-client"
+              className="btn-sm bg-gray-400 rounded-full pt-1 lg:btn lg:bg-gray-400 lg:rounded-full justify-end text-white lg:text-white"
+              rel="noopener noreferrer"
+            >
+              Github Link
+            </a>
           </div>
         </div>
+        {/* project2 */}
         <div className="rounded-md lg:w-96 bg-gray-500 bg-opacity-20 shadow-xl ">
-          <figure>
+          <figure className="h-60">
             <img
-              src="https://i.ibb.co/3St5p4t/service.png"
-              alt="Shoes"
+              src="https://i.ibb.co/TmzXqkt/service.png"
+              alt="Service"
               className="rounded-md"
             />
           </figure>
-          <div className="card-body">
-            <h2 className="card-title">Service Provide Website</h2>
+          <div className="card-body lg:-mt-5 h-56">
+            <h2 className="card-title ">Service Provide Website</h2>
             <p className="text-justify">
               This is a service provide website name Repair Service Hub. Some
               Top Features are - Log in, Registration service add, Service edit,
               Service update, Service delete. See my service and also see my
               booking service. See the Details.
             </p>
-
+          </div>
+          <div className="card-body flex-grow">
             <a
               href="https://fabulous-squirrel-c63dda.netlify.app/"
-              className="btn-sm bg-green-400 rounded-full pt-1 lg:btn lg:bg-green-400 lg:rounded-full justify-start text-black lg:text-black"
+              className="btn-sm bg-green-400 rounded-full pt-1 mb-1 lg:mb-0 lg:btn lg:bg-green-400 lg:rounded-full justify-start text-black lg:text-black lg:hover:text-white"
               rel="noopener noreferrer"
             >
               Live Link
@@ -73,128 +76,139 @@ const Projects = () => {
             </a>
           </div>
         </div>
+        {/* project3 */}
         <div className="rounded-md lg:w-96 bg-gray-500 bg-opacity-20 shadow-xl ">
-          <figure>
+          <figure className="h-60">
             <img
               src="https://i.ibb.co/SBqLsGC/car-brand-shop.png"
               alt="Shoes"
               className="rounded-md"
             />
           </figure>
-          <div className="card-body">
-            <h2 className="card-title">Brand Shop Portal</h2>
+          <div className="card-body lg:-mt-5 h-56">
+            <h2 className="card-title ">Brand Shop Portal</h2>
             <p className="text-justify">
               This is a Automotive project where try to create a car website
               with various brands. In Below, there is at least 5 points of my
               project - Navbar with Login registration 6 brands of famous car.
               Show specific car when click on brand name.
             </p>
-
+          </div>
+          <div className="card-body flex-grow">
             <a
               href="https://car-brand-shop.netlify.app"
-              className="btn-sm bg-orange-400 rounded-full pt-1 lg:btn lg:bg-orange-400 lg:rounded-full justify-start"
+              className="btn-sm bg-green-400 rounded-full pt-1 mb-1 lg:mb-0 lg:btn lg:bg-green-400 lg:rounded-full justify-start text-black lg:text-black lg:hover:text-white"
               rel="noopener noreferrer"
             >
               Live Link
             </a>
             <a
               href="https://github.com/AlMamunTalukder/brandshop-client"
-              className="btn-sm bg-orange-400 rounded-full pt-1 lg:btn lg:bg-orange-400 lg:rounded-full justify-end"
+              className="btn-sm bg-gray-400 rounded-full pt-1 lg:btn lg:bg-gray-400 lg:rounded-full justify-end text-white lg:text-white"
               rel="noopener noreferrer"
             >
               Github Link
             </a>
           </div>
         </div>
+        {/* project4 */}
         <div className="rounded-md lg:w-96 bg-gray-500 bg-opacity-20 shadow-xl ">
-          <figure>
+          <figure className="h-60">
             <img
-              src="https://i.ibb.co/zFhg08p/edutechpro9.png"
-              alt="Shoes"
+              src="https://i.ibb.co/FKtXL81/edutechpro9.png"
+              alt="EduTechPro"
               className="rounded-md"
             />
           </figure>
-          <div className="card-body">
-            <h2 className="card-title">Educational Events Management System</h2>
+          <div className="card-body lg:-mt-5 h-56">
+            <h2 className="card-title text-left ">
+              Educational Events Management System
+            </h2>
             <p className="text-justify">
               This website is educational events management system name
               edutechpro. Mainly has Some Features- - Educational events add. -
               show details - add to cart
             </p>
-
+          </div>
+          <div className="card-body flex-grow">
             <a
               href="https://edutechpro9.netlify.app/"
-              className="btn-sm bg-orange-400 rounded-full pt-1 lg:btn lg:bg-orange-400 lg:rounded-full justify-start"
+              className="btn-sm bg-green-400 rounded-full pt-1 mb-1 lg:mb-0 lg:btn lg:bg-green-400 lg:rounded-full justify-start text-black lg:text-black lg:hover:text-white"
               rel="noopener noreferrer"
             >
               Live Link
             </a>
             <a
               href="https://github.com/AlMamunTalukder/edu_tech_pro"
-              className="btn-sm bg-orange-400 rounded-full pt-1 lg:btn lg:bg-orange-400 lg:rounded-full justify-end"
+              className="btn-sm bg-gray-400 rounded-full pt-1 lg:btn lg:bg-gray-400 lg:rounded-full justify-end text-white lg:text-white"
               rel="noopener noreferrer"
             >
               Github Link
             </a>
           </div>
         </div>
+        {/* project5 */}
         <div className="rounded-md lg:w-96 bg-gray-500 bg-opacity-20 shadow-xl ">
-          <figure>
+          <figure className="h-60">
             <img
               src="https://i.ibb.co/t8k1fsr/donationcapm.png"
               alt="Shoes"
               className="rounded-md"
             />
           </figure>
-          <div className="card-body">
+          <div className="card-body lg:-mt-5 h-56">
             <h2 className="card-title">Donation Site</h2>
             <p className="text-justify">
               This is a donation website name donation campaign. The 3 project
               feature is. Show Data in cards, Details Page, Donates Cards show
               in donation page. Statastics, Search functionality.
             </p>
-
+          </div>
+          <div className="card-body flex-grow">
             <a
               href="http://donationcapm-2023.surge.sh/"
-              className="btn-sm bg-orange-400 rounded-full pt-1 lg:btn lg:bg-orange-400 lg:rounded-full justify-start"
+              className="btn-sm bg-green-400 rounded-full pt-1 mb-1 lg:mb-0 lg:btn lg:bg-green-400 lg:rounded-full justify-start text-black lg:text-black lg:hover:text-white"
               rel="noopener noreferrer"
             >
               Live Link
             </a>
             <a
               href="https://github.com/AlMamunTalukder/donation-campaign"
-              className="btn-sm bg-orange-400 rounded-full pt-1 lg:btn lg:bg-orange-400 lg:rounded-full justify-end"
+              className="btn-sm bg-gray-400 rounded-full pt-1 lg:btn lg:bg-gray-400 lg:rounded-full justify-end text-white lg:text-white"
               rel="noopener noreferrer"
             >
               Github Link
             </a>
           </div>
         </div>
+
+        {/* project6 */}
         <div className="rounded-md lg:w-96 bg-gray-500 bg-opacity-20 shadow-xl ">
-          <figure>
+          <figure className="h-60">
             <img
-              src="https://i.ibb.co/T4QbMgV/course.png"
-              alt="Shoes"
+              src="https://i.ibb.co/R6HDW0P/course.png"
+              alt="Course"
               className="rounded-md"
             />
           </figure>
-          <div className="card-body">
+          <div className="card-body lg:-mt-5 h-56">
             <h2 className="card-title">Course Registration</h2>
             <p className="text-justify">
               This is a newpaper website name global Times. It has many more
               features. Here is some features: Login, registration with google
             </p>
-
+          </div>
+          <div className="card-body flex-grow">
             <a
               href="https://course-r2023.surge.sh/"
-              className="btn-sm bg-orange-400 rounded-full pt-1 lg:btn lg:bg-orange-400 lg:rounded-full justify-start"
+              className="btn-sm bg-green-400 rounded-full pt-1 mb-1 lg:mb-0 lg:btn lg:bg-green-400 lg:rounded-full justify-start text-black lg:text-black lg:hover:text-white"
               rel="noopener noreferrer"
             >
               Live Link
             </a>
             <a
               href="https://github.com/AlMamunTalukder/course-registration"
-              className="btn-sm bg-orange-400 rounded-full pt-1 lg:btn lg:bg-orange-400 lg:rounded-full justify-end"
+              className="btn-sm bg-gray-400 rounded-full pt-1 lg:btn lg:bg-gray-400 lg:rounded-full justify-end text-white lg:text-white"
               rel="noopener noreferrer"
             >
               Github Link
