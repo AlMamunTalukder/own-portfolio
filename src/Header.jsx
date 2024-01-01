214069c18fa9d8a13f4b4f085411d41ba8d9cfd4
AlMamunTalukder <almamuntalukder0@gmail.com>
2024-01-02 04:15:52 +0600

    portfolio update

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,16 +4,16 @@ import { Typewriter } from "react-simple-typewriter";
 
 const Header = () => {
   return (
-    <div className="md:flex md:mt-20 md:gap-28 md:mx-16 lg:flex lg:mt-20 lg:gap-28 lg:mx-16 items-center content-center text-center py-10 gap-6">
-      <div className="text-left  lg:pr-14 ">
-        <h1 className="text-3xl lg:text-7xl mb-5 pl-6">
+    <div className="md:flex md:mt-20 md:gap-28 md:mx-16 lg:flex lg:mt-20 lg:gap-28 items-center content-center text-center lg:items-center lg:content-center lg:text-center md:py-10 lg:py-10 gap-6">
+      <div className="  items-center content-center text-center lg:items-center lg:content-center lg:text-left">
+        <h1 className="text-3xl lg:text-7xl lg:mb-5 ">
           Hello, I’m <br />
           <span className="text-orange-500 md:text-4xl lg:text-6xl">
             Al Mamun Talukder
           </span>
         </h1>
 
-        <h1 className="text-sm lg:text-2xl px-6">
+        <h1 className="text-sm lg:text-2xl mt-2">
           I am a{" "}
           <span className="font-serif text-green-500 font-bold">
             <Typewriter
@@ -32,18 +32,18 @@ const Header = () => {
           </span>
         </h1>
 
-        <div className="flex items-center lg:items-left">
+        <div className=" items-center content-center text-center lg:items-center lg:content-center lg:text-left">
           <a
             href="https://drive.google.com/file/d/1tkhqYwbrXNRBS5jdff28NfqI3Q-DuncK/view?usp=drive_link"
             target="_blank"
             rel="noopener noreferrer"
-            className="btn btn-outline btn-success my-5 text-xs ml-14 lg:text-lg"
+            className="btn btn-outline btn-success my-5 text-xs   lg:text-lg"
           >
             My Resume
           </a>
         </div>
       </div>
-      <div className="w-56 pl-20 lg:w-72">
+      <div className="w-52 lg:pl-20 lg:w-72 ml-16 md:ml-0 lg:ml-0">
         <img src={DP} alt="Talukder" />
       </div>
     </div>
